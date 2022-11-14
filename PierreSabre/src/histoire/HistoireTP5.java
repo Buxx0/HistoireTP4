a5296440fe0fd710925348b30bc261c8894e8420
K <konstantin.andjelkovic03@gmail.com>
2022-11-14 20:00:58 +0100

    mise a jour HistoireTP5

--- PierreSabre/src/histoire/HistoireTP5.java
+++ PierreSabre/src/histoire/HistoireTP5.java
@@ -10,7 +10,8 @@ public class HistoireTP5 {
 		Commercant kumi = new Commercant("Kumi", 10);
 		Yakuza yaku = new Yakuza("Yaku le noir", "whisky", 30, "Warsong");
 		Ronin roro = new Ronin("Roro", "shochu", 60);
-		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "sakï¿½", 80);
+		Traitre masako = new Traitre("Miyamoto", "Masako", "whisky", 100);
 		
 		marco.faireConnaisanceAvec(roro);
 		marco.faireConnaisanceAvec(yaku);
@@ -24,5 +25,17 @@ public class HistoireTP5 {
 		akimoto.faireConnaisanceAvec(marco);
 		akimoto.listerConnaissance();
 		akimoto.boire("the");
+		
+		System.out.println("\n\n\n");
+		masako.faireLeGentil();
+		masako.ranconner(kumi);
+		masako.ranconner(chonin);
+		masako.ranconner(marco);
+		akimoto.faireConnaisanceAvec(masako);
+		masako.ranconner(kumi);
+		masako.faireConnaisanceAvec(yaku);
+		masako.faireLeGentil();
+		masako.faireConnaisanceAvec(roro);
+		masako.listerConnaissance();
 	}
 }
